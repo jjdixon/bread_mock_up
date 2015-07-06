bb13ccd3c1a0b7b006d300ce422b0fc77539036a
Joshua Dixon <josdxn@yahoo.com>
2015-07-06 11:40:15 -0500

    1

--- js/main.js
+++ js/main.js
@@ -44,9 +44,9 @@ function formTableData(data, custom, alltours,simulateDate) {
 				
 				switchNum = today.getDay() + simulateDate;
 				console.log(switchNum);
-				switch (+switchNum) {
+				switch (switchNum) {
 				case 1: tour = "M";
-				case 2: tour = "R";
+				case "2": tour = "R";
 				case 3: tour = "T";
 				case 4: tour = "W";
 				//default: tour = "M";
