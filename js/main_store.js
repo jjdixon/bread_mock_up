38d36a1e09411e05273ae4b47d74304c600bf182
Joshua Dixon <josdxn@yahoo.com>
2015-07-13 16:19:17 -0500

    changed alert

--- js/main_store.js
+++ js/main_store.js
@@ -303,7 +303,7 @@ $(document).on('click','.desired', function(e){
 	var re = /.([0-9]+)/;
 	currentID= $(this).attr('id');
 	loc =  re.exec(currentID);
-	alert(loc[0]);
+	alert(loc[1]);
 	var x = goldenPath["Breakfast Meats"][0]-14;
 	var y = goldenPath["Breakfast Meats"][1]-14;
 	document.getElementById("golden").innerHTML += '<img src="'+desiredURL+'" style= "position:absolute;top:'+y+'px;left:'+x+'px;" />';
