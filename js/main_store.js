251a6d77924d6e69f69f1b2ed35c76d9255bf331
Joshua Dixon <josdxn@yahoo.com>
2015-07-13 17:25:42 -0500

    updated if statement

--- js/main_store.js
+++ js/main_store.js
@@ -318,7 +318,7 @@ $(document).on('click','.desired', function(e){
 	locID =  re.exec(currentID)[1];
 	location = $('#location'+locID).val();
 	
-	if($(this).prop('checked') && ($(location+'desiredURL').length < 0)){
+	if($(this).prop('checked') && ($(location+'desiredURL').length)){
 	var x = goldenPath[location][0]-14;
 	var y = goldenPath[location][1]-14;
 	document.getElementById("golden").innerHTML += '<img src="'+desiredURL+'" id="'+location+'desired" style= "position:absolute;top:'+y+'px;left:'+x+'px;" />';
