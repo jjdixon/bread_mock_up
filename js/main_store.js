a3f4710722752d6420878a61ebb996b21e1e4d7f
Joshua Dixon <josdxn@yahoo.com>
2015-07-16 14:24:59 -0500

    floor

--- js/main_store.js
+++ js/main_store.js
@@ -468,6 +468,6 @@ $(document).on('change','.linear',function(e){
 	var netdollars = document.getElementById('net'+id[1]).innerHTML;
 	var linearft = document.getElementById('lin'+id[1]).value;
 	console.log(linearft,parseInt(linearft),netdollars,parseInt(netdollars),$('#lin'+id).val();
-	document.getElementById("linper"+id[1]).innerHTML = (parseInt(netdollars)/parseInt(linearft)).toFixed(2);
+	document.getElementById("linper"+id[1]).innerHTML = Math.Floor(parseInt(netdollars)/parseInt(linearft));
 });
 
