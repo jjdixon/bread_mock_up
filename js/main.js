90028c3bd42fd2dafec5173fad339ac405a05eda
Joshua Dixon <josdxn@yahoo.com>
2015-07-22 13:14:44 -0500

    fixed pricing table

--- js/main.js
+++ js/main.js
@@ -265,7 +265,7 @@ function formPricingTable(data){
 	
 	if(data){
 		data.forEach(function(entry){
-		tableHTML = '<tr><td>'+entry.company+'</td><td>'+entry.Item+'</td><td>'+entry.price+'</td><td><select class="input-control"><option>Select Type</option>'+
+		tableHTML = '<tr><td>'+entry.company+'</td><td>'+entry.Item+'</td><td><input type="number"></td><td><select class="input-control"><option>Select Type</option>'+
 		'<option>Feature</option> <option>TPR</option><option>EDLP</option></select></td></tr>';
 		rows.push(tableHTML);
 		});
